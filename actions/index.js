4f1d3d25bb5ab1fd1328f0716938fabf98918bee
Hafeez Ghanchi <hafeezghanchi927@gmail.com>
2020-11-30 03:50:47 +0500

    refactored actions

--- actions/index.js
+++ actions/index.js
@@ -1,30 +1,15 @@
-export const RECEIVE_DECKS = "RECEIVE_DECKS"
-export const RECEIVE_DECK = "RECEIVE_DECK"
-export const ADD_DECK = "ADD_DECK"
-export const REMOVE_DECK = "REMOVE_DECK"
-export const ADD_CARD = "ADD_CARD"
-export const REMOVE_CARD = "REMOVE_CARD"
+import { getDecks } from '../utils/api';
 
-import {
-  getDecks as _getDecks,
-  saveDeck as _saveDeck,
-  getDeckByTitle as _getDeckByTitle,
-  deleteDeck as _deleteDeck,
-  addCard as _addCard } from "../utils/api"
+export const RECEIVE_DECKS = 'RECEIVE_DECKS';
+export const ADD_DECK = 'ADD_DECK';
+export const REMOVE_DECK = 'REMOVE_DECK';
+export const ADD_CARD = 'ADD_CARD';
+export const RESET_STORE = 'RESET_STORE';
 
-// Actions
-// --------
 export function receiveDecks(decks) {
   return {
     type: RECEIVE_DECKS,
     decks
-  };
-}
-
-export function receiveDeck(deck) {
-  return {
-    type: RECEIVE_DECK,
-    deck
   };
 }
 
@@ -34,80 +19,31 @@ export function addDeck(title) {
     title
   };
 }
-export function removeDeck(title) {
+
+export function removeDeck(id) {
   return {
     type: REMOVE_DECK,
-    title
+    id
   };
 }
 
-export function addCard(card) {
+export function addCardToDeck(deckId, card) {
   return {
     type: ADD_CARD,
+    deckId,
     card
   };
 }
 
-export function removeCard(deckQuestion) {
+export function resetStore() {
   return {
-    type: REMOVE_CARD,
-    deckQuestion
+    type: RESET_STORE
   };
 }
-
-// Handlers
-// ---------
-
 export function handleInitialData() {
   return dispatch => {
-    return _getDecks()
-      .then(data => {
-        dispatch(receiveDecks(JSON.parse(data)));
-      })
-      .catch(e => {
-        console.error(e);
-      });
-  };
-}
-
-export function handleAddDeck(title) {
-  return dispatch => {
-    return _saveDeck(title)
-      .then(() => {
-        dispatch(addDeck(title));
-      })
-      .catch(e => {
-        console.error(e);
-      });
-  };
-}
-
-export function handleReceiveDeck(title) {
-  return dispatch => {
-    return _getDeckByTitle(title)
-      .then(data => {
-        dispatch(receiveDeck(JSON.parse(data)));
-      })
-      .catch(e => console.error(e));
-  };
-}
-
-export function handleRemoveDeck(title) {
-  return dispatch => {
-    return _deleteDeck(title)
-      .then(() => {
-        dispatch(removeDeck(title));
-      })
-      .catch(e => console.error(e));
-  };
-}
-
-export function handleAddCard(card) {
-  return dispatch => {
-    return _addCard(card)
-      .then(() => {
-        dispatch(addCard(card));
-      })
-      .catch(e => console.error(e));
+    return getDecks().then(decks => {
+      dispatch(receiveDecks(decks));
+    });
   };
 }
