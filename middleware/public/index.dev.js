6cd870171170cb16a6e2db8052082c5945f9b3ef
Hafeez Ghanchi <hafeezghanchi927@gmail.com>
2020-12-02 15:54:39 +0500

    removed pkgs

--- middleware/public/index.dev.js
+++
@@ -1,1 +0,0 @@
-"use strict";var _interopRequireDefault=require("@babel/runtime/helpers/interopRequireDefault");Object.defineProperty(exports,"__esModule",{value:true});exports["default"]=void 0;var _redux=require("redux");var _reduxThunk=_interopRequireDefault(require("redux-thunk"));var _default=(0,_redux.applyMiddleware)(_reduxThunk["default"]);exports["default"]=_default;
